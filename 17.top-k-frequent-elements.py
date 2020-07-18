ea1867316c48a3e5255d297f68d9d0fedbe430cb
Mijeong Ban <mban1@stevens.edu>
2020-07-18 10:20:46 +0900

    071820

---
+++ 17.top-k-frequent-elements.py
@@ -0,0 +1,25 @@
+"""
+July 17, 2020
+Reverse Bits
+"""
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if nums == []:
+            return []
+        if len(nums) == k:
+            return nums
+        dic = {}
+        for i in range(len(nums)):
+            if nums[i] in dic:
+                dic[nums[i]] += 1
+            else:
+                dic[nums[i]] = 1
+                
+        sortedDic = sorted(dic.items(), key=lambda x: x[1], reverse=True)
+        result = []
+        for i in range(0, k):
+            result.append(sortedDic[i][0])
+        
+        return result
+
+
