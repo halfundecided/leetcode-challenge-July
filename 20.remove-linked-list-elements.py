f147d62b52a609292d8439939eb598eff40e98a5
Mijeong Ban <mban1@stevens.edu>
2020-07-21 10:25:43 +0200

    072120

---
+++ 20.remove-linked-list-elements.py
@@ -0,0 +1,15 @@
+"""
+July 20, 2020
+Remove Linked List Elements
+"""
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        
+
+
